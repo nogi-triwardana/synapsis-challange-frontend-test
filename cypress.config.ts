c107e2ad66f9c06f0e235636610282013e7ec2f8
Nogi Ragil Triwardana <nogi.triwardana@smt-id.com>
2025-01-03 18:39:03 +0700

    feat(cypress): improve baseUrl dynamically based from server👷

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'cypress'
+import { getBaseUrl } from './helpers';
  
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     supportFile: false,
-    baseUrl: "http://localhost:3000",
+    baseUrl: getBaseUrl(),
     defaultCommandTimeout: 10000,
   },
 });
--- helpers/index.tsx
+++ helpers/index.tsx
@@ -5,3 +5,14 @@ export function addProp<T extends object, K extends PropertyKey, V>(
 ): asserts obj is T & { [P in K]: V } {
   Object.assign(obj, { [key]: value });
 }
+
+export function getBaseUrl() {
+  const env = process.env.NEXT_PUBLIC_CYPRESS_ENV;
+  
+  switch(env) {
+    case 'production':
+      return 'https://synapsis-challange-frontend-test.vercel.app/';
+    default:
+      return 'http://localhost:3000';
+  }
+}
